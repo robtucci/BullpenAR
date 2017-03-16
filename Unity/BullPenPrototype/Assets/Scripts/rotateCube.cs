ad29388f8268f5784874765520567e90d238277f
Brian Kaufman <Brian Kaufman>
2017-03-16 14:30:06 -0500

    no message

---
+++ Unity/BullPenPrototype/Assets/Scripts/rotateCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateCube : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var eulers = transform.rotation.eulerAngles;
+        transform.Rotate(new Vector3(0.0f,0.1f,0f));
+        Debug.Log(eulers.x + ", " + eulers.y + ", " + eulers.z);
+	}
+}
